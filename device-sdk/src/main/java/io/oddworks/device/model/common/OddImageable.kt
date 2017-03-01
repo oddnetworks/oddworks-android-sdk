f216c358d04d28de6d5289706fd99fb880e19d8b
Erik Straub <brkattk@users.noreply.github.com>
2017-03-01 09:55:03 -0500

    Adds OddImageable interface for easier filtering

--- device-sdk/src/main/java/io/oddworks/device/model/common/OddImageable.kt
+++ device-sdk/src/main/java/io/oddworks/device/model/common/OddImageable.kt
@@ -3,39 +3,31 @@ package io.oddworks.device.model.common
 interface OddImageable {
     val images: Set<OddImage>
 
-    fun getImagesByMimeType(mimeType: String): List<OddImage> {
-        return images.filter {
-            it.mimeType == mimeType
-        }
+    fun getImagesByMimeType(mimeType: OddImage.MimeType): List<OddImage> {
+        return images.filter { it.mimeType == mimeType }
     }
 
     fun getImagesByLabel(label: String): List<OddImage> {
-        return images.filter {
-            it.label == label
-        }
+        return images.filter { it.label.contentEquals(label) }
+    }
+
+    fun getImagesByLabel(label: Regex) : List<OddImage> {
+        return images.filter { it.label.matches(label) }
     }
 
     fun getImagesByMaxWidth(width: Int): List<OddImage> {
-        return images.filter {
-            it.width <= width
-        }
+        return images.filter { it.width <= width }
     }
 
     fun getImagesByMaxHeight(height: Int): List<OddImage> {
-        return images.filter {
-            it.height <= height
-        }
+        return images.filter { it.height <= height }
     }
 
     fun getImagesByMinWidth(width: Int): List<OddImage> {
-        return images.filter {
-            it.width >= width
-        }
+        return images.filter { it.width >= width }
     }
 
     fun getImagesByMinHeight(height: Int): List<OddImage> {
-        return images.filter {
-            it.height >= height
-        }
+        return images.filter { it.height >= height }
     }
 }
