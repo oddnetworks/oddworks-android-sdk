84ffcf51790da0d3b25d8b48dd9dead111604f37
Erik Straub <brkattk@users.noreply.github.com>
2017-03-01 09:54:37 -0500

    Adds MimeType enum to OddImage with lookup

--- device-sdk/src/main/java/io/oddworks/device/model/common/OddImage.kt
+++ device-sdk/src/main/java/io/oddworks/device/model/common/OddImage.kt
@@ -1,3 +1,34 @@
 package io.oddworks.device.model.common
 
-data class OddImage (val url: String, val mimeType: String, val width: Int, val height: Int, val label: String)
+import android.util.Log
+
+data class OddImage (val url: String,
+                     val mimeType: MimeType,
+                     val width: Int,
+                     val height: Int,
+                     val label: String) {
+
+    enum class MimeType(val mimeType: String, val extensions: List<String>) {
+        BMP("image/bmp", listOf(".bmp")),
+        GIF("image/gif", listOf(".gif")),
+        JPEG("image/jpeg", listOf(".jpg")),
+        PNG("image/png", listOf(".png")),
+        WEBP("image/webp", listOf(".webp"));
+
+        companion object {
+            fun valueFromMimeType(mimeType: String) : MimeType? {
+                return when (mimeType) {
+                    "image/bmp" -> BMP
+                    "image/gif" -> GIF
+                    "image/jpeg", "image/jpg" -> JPEG
+                    "image/png" -> PNG
+                    "image/webp" -> WEBP
+                    else -> {
+                        Log.w(MimeType::class.java.simpleName, "Unknown MimeType $mimeType")
+                        null
+                    }
+                }
+            }
+        }
+    }
+}
